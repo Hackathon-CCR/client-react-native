c1496f3dea7864656f9cafd1979739c593e7cdac
jordan <jordan@gmail.com>
2020-06-13 09:09:00 -0300

    login-autenticacao

--- App.js
+++ App.js
@@ -1,53 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
-import Home from "./src/views/home/index";
-import Login from "./src/views/login/index";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "react-native";
-import Alerta from "./src/views/alerta/index";
-import Saude from "./src/views/saude";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import Home from './src/views/home/index';
+import Login from './src/views/login/index';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import Alerta from './src/views/alerta/index';
+import Saude from './src/views/saude';
 
-StatusBar.setBarStyle("light-content");
+StatusBar.setBarStyle('light-content');
 
 const Stack = createStackNavigator();
 
 const headerOptions = {
-  headerStyle: {
-    backgroundColor: "#A9432E",
-  },
-  headerTitleStyle: {
-    color: "#FFF",
-  },
-  headerTintColor: "#FFF",
+	headerStyle: {
+		backgroundColor: '#A9432E'
+	},
+	headerTitleStyle: {
+		color: '#FFF'
+	},
+	headerTintColor: '#FFF'
 };
 
 function App() {
-  return (
-    <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen options={headerOptions} name="Home" component={Home} />
-          <Stack.Screen
-            options={headerOptions}
-            name="Login"
-            component={Login}
-          />
-          <Stack.Screen
-            options={headerOptions}
-            name="Alerta"
-            component={Alerta}
-          />
-          <Stack.Screen
-            options={headerOptions}
-            name="Saude"
-            component={Saude}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
-  );
+	return (
+		<PaperProvider>
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName='Login'>
+					<Stack.Screen options={headerOptions} name='Home' component={Home} />
+					<Stack.Screen options={headerOptions} name='Login' component={Login} />
+					<Stack.Screen options={headerOptions} name='Alerta' component={Alerta} />
+					<Stack.Screen options={headerOptions} name='Saude' component={Saude} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</PaperProvider>
+	);
 }
 
 export default App;
--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,10 +1,85 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, Image, KeyboardAvoidingView } from 'react-native';
+import { Button } from 'react-native-paper';
+import { ScrollView } from 'react-native-gesture-handler';
+import { styles } from './styles';
+import { TextInput } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import Toast from 'react-native-tiny-toast'
+
 
 export default function Login() {
-  return (
-    <View>
-      <Text>Login work's</Text>
-    </View>
-  );
+	const [ login, setLogin ] = useState('');
+	const [ password, setPassword ] = useState('');
+
+	const navigation = useNavigation();
+
+	function handleLogin() {
+		if(login === "admin" && password === "admin") {
+			navigation.navigate('Home');
+		} else {
+			Toast.show('Falha ao autenticar, verifique seus dados!', {
+				containerStyle:{
+					backgroundColor: '#f00',
+					borderRadius: 15,
+				   },
+				   textStyle:{
+					color:'#fff',
+				   },
+				   duration: 2000,
+				   animation: true,
+			})
+		}
+	}
+
+	return (
+		<KeyboardAvoidingView 
+			style={{ flex: 1 }} 
+			behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+		>
+			<ScrollView>
+				<View style={styles.container}>
+					<View style={styles.logo}>
+						<Image source={require('../../../imagens/logo.png')} />
+					</View>
+				</View>
+				<View style={styles.inputArea}>
+					<Text style={styles.description}>Login</Text>
+
+					<TextInput
+						style={styles.input} 
+						placeholder='' 
+						autoCorrect={false} 
+						onChangeText={setLogin}
+					/>
+
+					<Text style={styles.description}>Senha</Text>
+
+					<TextInput
+						style={styles.input}
+						placeholder=''
+						autoCorrect={false}
+						onChangeText={setPassword}
+						secureTextEntry={true}
+					/>
+
+					<Text style={styles.forgot}>Esqueci a senha</Text>
+					</View>
+					<View style={styles.footer}>
+						<Button 
+							color='white' 
+							style={styles.mainButton} 
+							onPress={() => handleLogin()}>
+							ENTRAR
+						</Button>
+						<Button 
+							color='white' 
+							style={styles.secondButton}
+							onPress={() => handleLogin()}>
+							CADASTRAR
+						</Button>
+					</View>
+			</ScrollView>
+		</KeyboardAvoidingView>
+	);
 }
